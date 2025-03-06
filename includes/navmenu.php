f13114cad708fb14e26d68cefd76bac18a91fa52
Line-WH <linerooed.gmail.com>
2025-03-06 12:10:50 +0100

    refactored header og navbar så de bliver kaldt korrekt + stylet disse

---
+++ includes/navmenu.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<nav class="navbar fixed-bottom">
+    <form class="container-fluid justify-content-center">
+        <button class="btn" type="button">
+            <img src="" alt="home">
+        </button>
+
+        <button class="btn" type="button">
+        <img src="" alt="lager">
+        </button>
+
+        <button class="btn" type="button">
+            <img src="" alt="projeker">
+        </button>
+
+        <button class="btn" type="button">
+            <img src="" alt="læring">
+        </button>
+
+        <button class="btn" type="button">
+            <img src="" alt="fællesskab">
+        </button>
+
+    </form>
+</nav>
--- index.php
+++ index.php
@@ -10,23 +10,26 @@ require "settings/init.php";
 <head>
     <meta charset="utf-8">
     
-    <title>Sigende titel</title>
+    <title>Fiberly</title>
     
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
-    
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
-    
+
+    <link href="./css/styles.css" rel="stylesheet" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
-    <?php include 'header.php' ?>
-
-    <h1>Velkommen til Fiberly</h1>
-
-    <?php include 'navmenu.php' ?>
+<body class="bg-mesa">
+    <?php
+    include("includes/header.php");
+    include("includes/navmenu.php");
+    ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
