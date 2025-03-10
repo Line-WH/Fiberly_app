c3a6e644db0e474bcc3e21dff08a2c7112ffc237
nielsennice <Chn007@edu.zealand.dk>
2025-03-10 11:11:56 +0100

    Jeg har sat vores header og navbar ind på alle vores udersider så de nu ser ens ud

--- lager.php
+++ lager.php
@@ -1,20 +1,36 @@
 <?php
-include ("includes/header.php");
-include ("includes/navmenu.php");
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Fiberly</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="./css/styles.css" rel="stylesheet" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-mesa">
+<?php
+include("includes/header.php");
+include("includes/navmenu.php");
 ?>
 
-<div class="container w-25">
-    <h5> Det har du på lager</h5>
-    <ul class="list-group gap-2">
-        <br>
-        <li class="list-group-item text-center"><img src="..." class="rounded float-start" alt="...">Friends cotton <br>100 m</li> <br>
-        <li class="list-group-item text-center">Highland wool <br> 50 m</li> <br>
-        <li class="list-group-item text-center">Gosy -Go handmade <br> 200 m</li> <br>
-        <li class="list-group-item text-center">Rainbow lace <br> 10 m</li> <br>
-        <li class="list-group-item text-center">Alpaca silk <br>5 m</li> <br>
-        <li class="list-group-item text-center">Fluffy day <br>100 m</li> <br>
-        <li class="list-group-item text-center">Tea time <br> 50 m</li> <br>
-
-    </ul>
-
-</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- projekter.php
+++ projekter.php
@@ -1,34 +1,37 @@
 <?php
-include ("includes/header.php");
-include ("includes/navmenu.php");
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Fiberly</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="./css/styles.css" rel="stylesheet" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-mesa">
+<?php
+include("includes/header.php");
+include("includes/navmenu.php");
 ?>
 
-<div class="container">
-
-    <div class="card" style="width: 18rem;">
-        <img src="https://images.unsplash.com/photo-1543334270-24bb46642afe?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHlhcm58ZW58MHx8MHx8fDI%3D" class="card-img-top" alt="np">
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="https://images.unsplash.com/photo-1543334270-24bb46642afe?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHlhcm58ZW58MHx8MHx8fDI%3D" class="card-img-top" alt="np">
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="https://images.unsplash.com/photo-1543334270-24bb46642afe?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHlhcm58ZW58MHx8MHx8fDI%3D" class="card-img-top" alt="np">
-    </div>
-    <br>
-
-</div>
-
-<h3 class="text-center">-Forårs sweater-</h3>
-<div class="container">
-    <ul class="list-group gap-2"
-    <li class="list-group-item"></li>
-    <br>
-    <li class="list-group-item text-center"><img src="..." class="rounded float-start" alt="...">Amigo chunky </li> <br>
-    <li class="list-group-item text-center">Pindestørrelse: 7mm</li> <br>
-    <li class="list-group-item text-center">Du er nået hertil:<br> Række 27</li> <br>
-
-    </ul>
-
-
-</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
 
