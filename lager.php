e7522b8d14ab6f5ccac3644c926ce1f7b004d077
nielsennice <Chn007@edu.zealand.dk>
2025-03-10 10:20:36 +0100

    forsøg på opsætning af lager samt nuværende projekter med lidt simpel styling i scss

--- lager.php
+++ lager.php
@@ -3,3 +3,18 @@ include ("includes/header.php");
 include ("includes/navmenu.php");
 ?>
 
+<div class="container w-25">
+    <h5> Det har du på lager</h5>
+    <ul class="list-group gap-2">
+        <br>
+        <li class="list-group-item text-center"><img src="..." class="rounded float-start" alt="...">Friends cotton <br>100 m</li> <br>
+        <li class="list-group-item text-center">Highland wool <br> 50 m</li> <br>
+        <li class="list-group-item text-center">Gosy -Go handmade <br> 200 m</li> <br>
+        <li class="list-group-item text-center">Rainbow lace <br> 10 m</li> <br>
+        <li class="list-group-item text-center">Alpaca silk <br>5 m</li> <br>
+        <li class="list-group-item text-center">Fluffy day <br>100 m</li> <br>
+        <li class="list-group-item text-center">Tea time <br> 50 m</li> <br>
+
+    </ul>
+
+</div>
--- projekter.php
+++ projekter.php
@@ -2,3 +2,33 @@
 include ("includes/header.php");
 include ("includes/navmenu.php");
 ?>
+
+<div class="container">
+
+    <div class="card" style="width: 18rem;">
+        <img src="https://images.unsplash.com/photo-1543334270-24bb46642afe?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHlhcm58ZW58MHx8MHx8fDI%3D" class="card-img-top" alt="np">
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://images.unsplash.com/photo-1543334270-24bb46642afe?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHlhcm58ZW58MHx8MHx8fDI%3D" class="card-img-top" alt="np">
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://images.unsplash.com/photo-1543334270-24bb46642afe?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHlhcm58ZW58MHx8MHx8fDI%3D" class="card-img-top" alt="np">
+    </div>
+    <br>
+
+</div>
+
+<h3 class="text-center">-Forårs sweater-</h3>
+<div class="container">
+    <ul class="list-group gap-2"
+    <li class="list-group-item"></li>
+    <br>
+    <li class="list-group-item text-center"><img src="..." class="rounded float-start" alt="...">Amigo chunky </li> <br>
+    <li class="list-group-item text-center">Pindestørrelse: 7mm</li> <br>
+    <li class="list-group-item text-center">Du er nået hertil:<br> Række 27</li> <br>
+
+    </ul>
+
+
+</div>
+
