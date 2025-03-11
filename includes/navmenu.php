9690ce19db880bde7ce6d22a746b90c628cc4a99
nadjaarnsted <nadjaarnsted@outlook.dk>
2025-03-11 08:24:03 +0100

    Rigtig link tilføjet til navigation

--- includes/navmenu.php
+++ includes/navmenu.php
@@ -4,30 +4,30 @@
 <nav class="navbar fixed-bottom">
     <form class="container navbar-container">
 
-        <button class="btn custom-btn" type="button">
+        <a class="btn custom-btn" href="index.php" >
             <img src="../images/hjem.png" alt="home">
             <span>Hjem</span>
-        </button>
+        </a>
 
-        <button class="btn custom-btn" type="button">
+        <a class="btn custom-btn" href="lager.php">
             <img src="../images/lager.png" alt="lager">
             <span>Lager</span>
-        </button>
+        </a>
 
-        <button class="btn custom-btn" type="button">
+        <a class="btn custom-btn" href="projekter.php">
             <img src="../images/projekter.png" alt="projeker">
             <span>Projekter</span>
-        </button>
+        </a>
 
-        <button class="btn custom-btn" type="button">
+        <a class="btn custom-btn" href="laer.php">
             <img src="../images/læring.png" alt="læring">
             <span>Lær</span>
-        </button>
+        </a>
 
-        <button class="btn custom-btn" type="button">
+        <a class="btn custom-btn" href="faellesskab.php">
             <img src="../images/community.png" alt="fællesskab">
             <span>Fællesskab</span>
-        </button>
+        </a>
 
     </form>
 </nav>
